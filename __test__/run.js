ebd21d0b62ad82a7cbbef9d8394b7962da2eedff
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2022-01-21 20:56:26 +0200

    tokenizer basic implementation

--- __test__/run.js
+++ __test__/run.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const { Parser } = require("../src/Parser");
 
 const parser = new Parser();
-const program = `42`;
+const program = `"hello"`;
 const ast = parser.parse(program);
 
 console.log(JSON.stringify(ast, null, 2));
--- src/Parser.js
+++ src/Parser.js
@@ -1,28 +1,70 @@
+const { Tokenizer } = require("./Tokenizer");
+
 class Parser {
+  constructor() {
+    this._string = "";
+    this._tokenizer = new Tokenizer();
+  }
+
   parse(string) {
     this._string = string;
+    this._tokenizer.init(string);
+
+    this._lookahead = this._tokenizer.getNextToken();
 
     return this.Program();
   }
 
-  /*
- Main entry point
- Program
-    : NumericLiteral
-    ;
-
-
-*/
-
   Program() {
-    return this.NumericLiteral();
+    return {
+      type: "Program",
+      body: this.Literal(),
+    };
+  }
+
+  Literal() {
+    switch (this._lookahead.type) {
+      case "NUMBER":
+        return this.NumericLiteral();
+      case "STRING":
+        return this.StringLiteral();
+    }
+    throw new SyntaxError(`Literal: unexpected literal production`);
+  }
+
+  StringLiteral() {
+    const token = this._eat("STRING");
+    return {
+      type: "StringLiteral",
+      value: token.value.slice(1, -1),
+    };
   }
 
   NumericLiteral() {
+    const token = this._eat("NUMBER");
     return {
       type: "NumericLiteral",
-      value: Number(this._string),
+      value: Number(token.value),
     };
+  }
+
+  _eat(tokenType) {
+    const token = this._lookahead;
+
+    if (token === null) {
+      throw new SyntaxError(
+        `Unexpected end of input, expected: "${tokenType}"`
+      );
+    }
+
+    if (token.type !== tokenType) {
+      throw new SyntaxError(
+        `Unexpected token: "${token.value}", expected: "${tokenType}"`
+      );
+    }
+
+    this._lookahead = this._tokenizer.getNextToken();
+    return token;
   }
 }
 
