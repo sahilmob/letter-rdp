09e8e3ed568ae8bea597422df833bcd3b1ffc55e
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2022-02-13 21:07:07 +0200

    parser implementation start

--- __test__/run.js
+++ __test__/run.js
@@ -1,42 +1,45 @@
 const assert = require("assert");
 const { Parser } = require("../src/Parser");
 
-const tests = [
-  require("./literals.test"),
-  require("./statement-list.test"),
-  require("./block.test"),
-  require("./emptyStatement.test"),
-  require("./math.test"),
-];
+// const tests = [
+//   require("./literals.test"),
+//   require("./statement-list.test"),
+//   require("./block.test"),
+//   require("./emptyStatement.test"),
+//   require("./math.test"),
+// ];
 const parser = new Parser();
+const program = `42`;
+const ast = parser.parse(program);
+console.log(JSON.stringify(ast, null, 2));
 
-function exec() {
-  const program = `   
-// A comment
+// function exec() {
+//   const program = `
+// // A comment
 
-/**
- * 
- * 
- * Documentation comment
- * 
- */ 
-// String
-'hello';
+// /**
+//  *
+//  *
+//  * Documentation comment
+//  *
+//  */
+// // String
+// 'hello';
 
-// Number
-42;
-`;
-  const ast = parser.parse(program);
+// // Number
+// 42;
+// `;
+//   const ast = parser.parse(program);
 
-  console.log(JSON.stringify(ast, null, 2));
-}
+//   console.log(JSON.stringify(ast, null, 2));
+// }
 
-// exec();
+// // exec();
 
-function test(program, expected) {
-  const ast = parser.parse(program);
-  assert.deepEqual(ast, expected);
-}
+// function test(program, expected) {
+//   const ast = parser.parse(program);
+//   assert.deepEqual(ast, expected);
+// }
 
-tests.forEach((testRun) => testRun(test));
-console.log("All assertions passed");
+// tests.forEach((testRun) => testRun(test));
+// console.log("All assertions passed");
--- src/Parser.js
+++ src/Parser.js
@@ -1,196 +1,28 @@
-const { Tokenizer } = require("./Tokenizer");
-
-const DefaultFactory = {
-  Program(body) {
-    return {
-      type: "Program",
-      body,
-    };
-  },
-  EmptyStatement() {
-    return {
-      type: "EmptyStatement",
-    };
-  },
-  BlockStatement(body) {
-    return { type: "BlockStatement", body };
-  },
-  ExpressionStatement(expression) {
-    return {
-      type: "ExpressionStatement",
-      expression,
-    };
-  },
-  StringLiteral(token) {
-    return {
-      type: "StringLiteral",
-      value: token.value.slice(1, -1),
-    };
-  },
-  NumericLiteral(token) {
-    return {
-      type: "NumericLiteral",
-      value: Number(token.value),
-    };
-  },
-};
-
-const AST_MODE = "default";
-
-const factory = AST_MODE === "default" ? DefaultFactory : null; // could s-expression ast;
-
 class Parser {
-  constructor() {
-    this._string = "";
-    this._tokenizer = new Tokenizer();
-  }
-
   parse(string) {
     this._string = string;
-    this._tokenizer.init(string);
-
-    this._lookahead = this._tokenizer.getNextToken();
-
     return this.Program();
   }
 
+  // Program
+  //   :NumericLiteral
+  //   ;
+
   Program() {
-    return factory.Program(this.StatementList());
+    return this.NumericLiteral();
   }
 
-  StatementList(stopLookahead = null) {
-    const statementList = [this.Statement()];
-
-    while (this._lookahead !== null && this._lookahead.type !== stopLookahead) {
-      statementList.push(this.Statement());
-    }
-
-    return statementList;
-  }
-
-  Statement() {
-    switch (this._lookahead.type) {
-      case ";":
-        return this.EmptyStatement();
-      case "{":
-        return this.BlockStatement();
-      default:
-        return this.ExpressionStatement();
-    }
-  }
-
-  EmptyStatement() {
-    this._eat(";");
-    return factory.EmptyStatement();
-  }
-
-  BlockStatement() {
-    this._eat("{");
-
-    const body = this._lookahead.type !== "}" ? this.StatementList("}") : [];
-
-    this._eat("}");
-
-    return factory.BlockStatement(body);
-  }
-
-  ExpressionStatement() {
-    const expression = this.Expression();
-    this._eat(";");
-    return factory.ExpressionStatement(expression);
-  }
-
-  Expression() {
-    return this.AdditiveExpression();
-  }
-
-  AdditiveExpression() {
-    return this._BinaryExpression(
-      "MultiplicativeExpression",
-      "ADDITIVE_OPERATOR"
-    );
-  }
-
-  MultiplicativeExpression() {
-    return this._BinaryExpression(
-      "PrimaryExpression",
-      "MULTIPLICATIVE_OPERATOR"
-    );
-  }
-
-  _BinaryExpression(builderName, operatorToken) {
-    let left = this[builderName]();
-
-    while (this._lookahead.type === operatorToken) {
-      const operator = this._eat(operatorToken).value;
-
-      const right = this[builderName]();
-
-      left = {
-        type: "BinaryExpression",
-        operator,
-        left,
-        right,
-      };
-    }
-
-    return left;
-  }
-
-  PrimaryExpression() {
-    switch (this._lookahead.type) {
-      case "(":
-        return this.ParenthesizedExpression();
-      default:
-        return this.Literal();
-    }
-  }
-
-  ParenthesizedExpression() {
-    this._eat("(");
-    const expression = this.Expression();
-    this._eat(")");
-    return expression;
-  }
-
-  Literal() {
-    switch (this._lookahead.type) {
-      case "NUMBER":
-        return this.NumericLiteral();
-      case "STRING":
-        return this.StringLiteral();
-    }
-    throw new SyntaxError(`Literal: unexpected literal production`);
-  }
-
-  StringLiteral() {
-    const token = this._eat("STRING");
-    return factory.StringLiteral(token);
-  }
-
+  // NumericLiteral
+  //   :NUMBER
+  //   ;
   NumericLiteral() {
-    const token = this._eat("NUMBER");
-    return factory.NumericLiteral(token);
-  }
-
-  _eat(tokenType) {
-    const token = this._lookahead;
-
-    if (token === null) {
-      throw new SyntaxError(
-        `Unexpected end of input, expected: "${tokenType}"`
-      );
-    }
-
-    if (token.type !== tokenType) {
-      throw new SyntaxError(
-        `Unexpected token: "${token.value}", expected: "${tokenType}"`
-      );
-    }
-
-    this._lookahead = this._tokenizer.getNextToken();
-    return token;
+    return {
+      type: "NumericLiteral",
+      value: Number(this._string),
+    };
   }
 }
 
-module.exports = { Parser };
+module.exports = {
+  Parser,
+};
